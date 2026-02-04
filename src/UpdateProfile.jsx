31df23d08bcd381e3ffd83838485da5af7eec37f
ABISHEK ANTONY <abishek8673@gmail.com>
2026-02-04 11:08:26 +0530

    Update UpdateProfile.jsx

--- src/UpdateProfile.jsx
+++ src/UpdateProfile.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
  
-function UpdateProfile() {
+function UpdateProfile() { 
   const [profile, setProfile] = useState(null)
   const navigate = useNavigate()
  
